66b547ec30b1cdf8ebbe7764c6477c1398534a34
khoironi2 <khoironi7887@students.amikom.ac.id>
2020-11-04 10:04:24 +0700

    register users

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -19,6 +19,8 @@
                                 </div>
                                 <button class="au-btn au-btn--block btn-dark m-b-20" type="submit">Login</button>
                             </form>
+
+                            <a href="" data-toggle="modal" data-target="#exampleModal">Register</a>
                         </div>
                     </div>
                 </div>
@@ -26,3 +28,36 @@
         </div>
     </div>
 </body>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Register User</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form class="form-signin" action="<?= base_url('auth/registerForm') ?>" method="post">
+                    <h1 class="h3 mb-3 font-weight-normal">Registration</h1>
+
+                    <input name="name" type="text" class="form-control" placeholder="nama lengkap" required autofocus>
+                    <div style="margin-top:10px"></div>
+                    <input name="email" type="email" id="inputEmail" class="form-control" placeholder="alamatemail@gmail.com" required autofocus>
+                    <div style="margin-top:10px"></div>
+                    <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                    <div style="margin-top:10px"></div> <input name="confrim_password" type="password" id="inputPassword" class="form-control" placeholder="Confrim Password" required>
+
+                    <br>
+
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Save changes</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
