d7618764ffa46d0772e1b1647ec6fa604ffe5eda
abdulrosyidit <abdulrosyidit@gmail.com>
2020-11-07 08:06:20 +0700

    Datatable

--- application/views/nasabah/index.php
+++ application/views/nasabah/index.php
@@ -22,7 +22,7 @@
     <div class="col-md-12">
         <!-- DATA TABLE-->
         <div class="table-responsive m-b-40">
-            <table class="table table-borderless table-data3">
+            <table class="table table-borderless table-data3" id="datatable">
                 <thead>
                     <tr>
                         <th>No</th>
